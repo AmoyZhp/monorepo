857e03a44024702f74788d157ad4cbb09726cd2f
housezam <whitehawk15@hotmail.com>
2022-05-29 17:13:22 +0800

    tutorial: http multi part read meta data done

--- tutorial/http_multipart.go
+++ tutorial/http_multipart.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 )
+
+// MetaData http meta-data form 携带的元数据
+type MetaData struct {
+	RequestID string `json:"request_id"`
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	reader, err := r.MultipartReader()
@@ -16,6 +24,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Println("get metadata part error: ", err.Error())
 		return
 	}
+	readMetaData(metadata)
 	log.Println("metadata part name: ", metadata.FormName())
 	filepart, err := reader.NextPart()
 	if err != nil {
@@ -23,6 +32,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println("file part name: ", filepart.FormName())
+	w.Write([]byte("request successfule"))
+}
+
+func readMetaData(part *multipart.Part) {
+	dataCache := make([]byte, 100)
+	n, err := part.Read(dataCache)
+	if err != nil && err != io.EOF {
+		log.Println("read meta data faile. ", err.Error())
+		return
+	}
+	data := &MetaData{}
+	err = json.Unmarshal(dataCache[:n], data)
+	if err != nil {
+		log.Println("json unmarshal faile. ", err.Error())
+		return
+	}
+	log.Printf("read meta data %+v", data)
+}
+
+func readFile(part *multipart.Part) {
+
 }
 
 func main() {
