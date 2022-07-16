bb81a4d3a99a106431dad5e07175f59759b5e245
housezam <whitehawk15@hotmail.com>
2022-07-16 23:05:31 +0800

    test: add unit test for GetPlayerAtPos & Reset

--- gomoku/executor/board_test.go
+++ gomoku/executor/board_test.go
@@ -31,14 +31,14 @@ func TestGomokuBoard_Set(t *testing.T) {
 				false,
 			},
 			{
-				"invalid colum",
+				"invalid row",
 				args{
 					Move{19, 7, BLACK},
 				},
 				true,
 			},
 			{
-				"invalid row",
+				"invalid col",
 				args{
 					Move{7, 19, BLACK},
 				},
@@ -57,6 +57,81 @@ func TestGomokuBoard_Set(t *testing.T) {
 				imp := NewGomokuBoard()
 				imp.Set(Move{8, 8, BLACK})
 				err := imp.Set(tt.args.move)
+				So(err != nil, ShouldEqual, tt.wantErr)
+			})
+		}
+	})
+}
+
+func TestGomokuBoard_Reset(t *testing.T) {
+	Convey("TestGomokuBoard_Reset", t, func() {
+		Convey("reset", func() {
+			innerBoard := make([][]Player, GomokuRow)
+			for i := 0; i < GomokuRow; i++ {
+				innerBoard[i] = make([]Player, GomokuCol)
+			}
+			historyMove := make([]Move, GomokuCol*GomokuRow)
+			board := &GomokuBoard{
+				board:       innerBoard,
+				historyMove: historyMove,
+				maxCol:      GomokuCol,
+				maxRow:      GomokuRow,
+				timeline:    0,
+			}
+			board.Set(Move{7, 7, BLACK})
+			board.Reset()
+			player, err := board.GetPlayerAtPos(7, 7)
+			So(player, ShouldEqual, EMPTY)
+			So(err, ShouldBeNil)
+			So(board.timeline, ShouldEqual, 0)
+			So(board.historyMove, ShouldResemble, historyMove)
+
+		})
+
+	})
+}
+
+func TestGomokuBoard_GetPlayerAtPos(t *testing.T) {
+	Convey("TestGomokuBoard_GetPlayerAtPos", t, func() {
+		type args struct {
+			row int
+			col int
+		}
+		tests := []struct {
+			name    string
+			args    args
+			want    Player
+			wantErr bool
+		}{
+			{
+				"normal",
+				args{7, 7},
+				BLACK,
+				false,
+			},
+			{
+				"invalid row",
+				args{
+					19, 7,
+				},
+				EMPTY,
+				true,
+			},
+			{
+				"invalid col",
+				args{
+					7, 19,
+				},
+				EMPTY,
+				true,
+			},
+		}
+		for _, tt := range tests {
+			Convey(tt.name, func() {
+				imp := NewGomokuBoard()
+				imp.Set(Move{7, 7, BLACK})
+				got, err := imp.GetPlayerAtPos(tt.args.row, tt.args.col)
+				So(got, ShouldEqual, tt.want)
 				So(err != nil, ShouldEqual, tt.wantErr)
 			})
 		}
